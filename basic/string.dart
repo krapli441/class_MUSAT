3a6d0846c527e0fdff89500efa6630c7848cb684
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 16:29:38 +0900

    string, runtimetype

--- basic/string.dart
+++ basic/string.dart
@@ -3,6 +3,13 @@ void main() {
   // 글자 타입
 
   String name = "John Wick";
-
   print(name);
+
+  // var String?
+  var name2 = "serato";
+  var number = 20;
+
+  print(name2.runtimeType);
+  print(number.runtimeType);
+  // 변수가 실행되는 순간 어떤 타입인지 runtimeType을 통해 알 수 있음.
 }
