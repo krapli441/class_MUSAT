e671d1dc03dd13ce6b22baa6b6b4edc7f455dbc3
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 16:32:27 +0900

    added string

--- basic/string.dart
+++ basic/string.dart
@@ -12,4 +12,9 @@ void main() {
   print(name2.runtimeType);
   print(number.runtimeType);
   // 변수가 실행되는 순간 어떤 타입인지 runtimeType을 통해 알 수 있음.
+
+  String one = "Hello";
+  String two = "World";
+
+  print(one + two);
 }
