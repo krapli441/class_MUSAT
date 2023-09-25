dbd2c05b0f84668e0a0428346ea0b310908f1bdb
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 17:43:28 +0900

    lIST

--- basic/list.dart
+++ basic/list.dart
@@ -1,10 +1,13 @@
 void main() {
   // List
   // 리스트
-
-  List<String> rifle = ['hk416', 'SCAR-L'];
+  List<String> rifle = ['hk416', 'SCAR-L', 'M249', 'AKM', 'FN FAL'];
   print(rifle);
-
+  print(rifle[0]);
+  print(rifle[1]);
+  print(rifle[2]);
+  print(rifle[3]);
+  print(rifle[4]);
   List<int> number = [1, 2, 3, 4, 5, 6];
   print(number);
 }
