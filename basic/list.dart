bc75bd759f364ce4198a97bf458749919d37b4cf
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 17:41:34 +0900

    list

---
+++ basic/list.dart
@@ -0,0 +1,10 @@
+void main() {
+  // List
+  // 리스트
+
+  List<String> rifle = ['hk416', 'SCAR-L'];
+  print(rifle);
+
+  List<int> number = [1, 2, 3, 4, 5, 6];
+  print(number);
+}
--- basic/operatorTwo.dart
+++ basic/operatorTwo.dart
@@ -8,4 +8,26 @@ void main() {
   print(number1 <= number2);
   print(number1 == number2);
   print(number1 != number2);
+
+  print('------------------');
+
+  print(number1 is int);
+  print(number1 is String);
+  print(number1 is! int);
+  print(number1 is! String);
+
+  // && 조건 - 모든 조건을 만족해야 true 반환
+  // || 조건 - 일부 조건만 만족해도 true 반환
+
+  bool result = 12 > 10 && 1 > 0;
+  print(result);
+
+  bool result2 = 12 > 10 && 0 > 1;
+  print(result2);
+
+  bool result3 = 12 > 10 || 1 > 0;
+  print(result3);
+
+  bool result4 = 12 > 10 || 0 > 1;
+  print(result4);
 }
