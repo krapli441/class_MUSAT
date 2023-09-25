eef7d2ff7dded17bc48a0f7a9ab31ef557354892
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 17:11:14 +0900

    operator

---
+++ basic/operator.dart
@@ -0,0 +1,18 @@
+void main() {
+  int number = 2;
+  print(number);
+  print(number + 2);
+  print('-------------------------');
+  print(number % 2);
+  print(number % 3);
+  print('-------------------------');
+  number++;
+  print(number);
+  number--;
+  print(number);
+  print('-------------------------');
+  double doubleNumber = 2.5;
+  print(doubleNumber);
+  doubleNumber += 1; // 원하는 숫자만큼 더할 수 있다.
+  print(doubleNumber);
+}
