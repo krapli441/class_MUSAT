ddd4d83cce85bda8c92398ff84d3301373e43ff7
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 16:34:59 +0900

    template string

--- basic/string.dart
+++ basic/string.dart
@@ -11,6 +11,8 @@ void main() {
 
   print(name2.runtimeType);
   print(number.runtimeType);
+  print(
+      '$name2의 타입은 ${name2.runtimeType}이고, $number의 타입은 ${number.runtimeType}입니다');
   // 변수가 실행되는 순간 어떤 타입인지 runtimeType을 통해 알 수 있음.
 
   String one = "Hello";
