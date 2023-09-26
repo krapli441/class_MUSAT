c9430eef0535114cc4f2cb5a51ba35e94d0c349a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-26 14:15:46 +0900

    static

--- oop/class-overriding.dart
+++ oop/class-overriding.dart
@@ -1,7 +1,8 @@
 void main() {
   TimesTwo tt = TimesTwo(2);
-
   print(tt.calculate());
+  TimesFour tf = TimesFour(4);
+  print(tf.calculate());
 }
 
 // method - class 내부에 있는 함수(function);
@@ -15,4 +16,20 @@ class TimesTwo {
   int calculate() {
     return this.number * 2;
   }
+}
+
+class TimesFour extends TimesTwo {
+  TimesFour(int number) : super(number);
+
+  // 상속받은 클래스의 메서드를 override를 통해 덮어씌웠음.
+  @override
+  int calculate() {
+    return super.number * 4;
+  }
+
+  // 상속받은 클래스의 메서드를 override를 통해 덮어씌웠음.
+  // @override
+  // int calculate() {
+  //   return super.calculate() * 2; // 부모 클래스의 메서드를 바로 불러와도 사용 가능함.
+  // }
 }
---
+++ oop/class-static.dart
@@ -0,0 +1,7 @@
+void main() {
+
+}
+
+class Employee {
+  static String? building;
+}
